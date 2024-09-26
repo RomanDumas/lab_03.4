10c5868a1b324415ac84854891891fd12d35d593
RomanDumas <dumasroman1011@gmail.com>
2024-09-26 18:29:31 +0300

    second

--- LR_3.4/LR_3.4.cpp
+++ LR_3.4/LR_3.4.cpp
@@ -5,10 +5,29 @@ using namespace std;
  
 int main() {
 	double x, y, R;
+	int f1, f2, f3;
 	cout << "x = "; cin >> x;
 	cout << "y = "; cin >> y;
 	cout << "R = "; cin >> R;
-	if ((y = sqrt(R * R - x * x) && x >= 0 && y >= -R && y <= R && x <= R) || (y = x && y >= -R && x <= 0 && y <= 0) || (y = -x && x <= 0 && y <= R && y >= 0))
+	if ((-R <= x && x <= 0) && (0 <= y && y <= R) && (-x <= y)) {
+		f1 = 1;
+	}
+	else {
+		f1 = 0;
+	}
+	if ((-R <= x && x <= 0) && (-R <= y && y <= 0) && (y <= x)) {
+		f2 = 1;
+	}
+	else {
+		f2 = 0;
+	}
+	if ((0 <= x && x <= R) && (-R <= y && y <= R)) {
+		f3 = 1;
+	}
+	else {
+		f3 = 0;
+	}
+	if (f1 || f2 || f3 == 1) 
 		cout << "yes" << endl;
 	else
 		cout << "no" << endl;
